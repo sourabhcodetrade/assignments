f98c99a68596cac428774f1417d82758f7470c61
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-04 11:47:05 +0530

    Refractored Custom Button

---
+++ lib/app/modules/auth/view/widgets/custom_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../controller/auth_controller.dart';
+
+class Button extends StatelessWidget {
+  const Button({
+    super.key,
+    required this.formKey,
+    required this.passwordController,
+    required this.authController,
+    required this.emailController,
+    required this.text,
+    required this.onPressed,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController passwordController;
+  final AuthController authController;
+  final TextEditingController emailController;
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.black87),
+      ),
+      onPressed: onPressed,
+      child:  Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
