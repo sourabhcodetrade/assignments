11e0f3fc35c7dd5ccf89ea290cf91890a25c920e
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-04 12:46:44 +0530

    removed unnecessary comments

--- lib/app/modules/auth/models/user.dart
+++ lib/app/modules/auth/models/user.dart
@@ -5,19 +5,5 @@ class User {
 
   User({required this.email, required this.uid});
 
-  //App(MyUser) - firebase
-  // Map<String, dynamic> toJson() => {
-  //       "email": email,
-  //       "password": uid,
-  //     };
-
-// firebase - App(MyUser)
-//   static User fromSnap(DocumentSnapshot snap) {
-//     var snapshot = snap.data() as Map<String, dynamic>;
-//
-//     return User(
-//         email: snapshot["email"],
-//         password: snapshot["password"],
-//         uid: snapshot["uid"]);
-//   }
+  
 }
