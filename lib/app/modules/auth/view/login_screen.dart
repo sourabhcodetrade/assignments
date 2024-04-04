1131af0d859b87d024049623dcdcb5d8cae3bf8e
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-04 11:47:38 +0530

    Fixed Warning

--- lib/app/modules/auth/view/login_screen.dart
+++ lib/app/modules/auth/view/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mvc/app/modules/auth/controller/auth_controller.dart';
 import 'package:mvc/app/modules/auth/view/widgets/custom_button.dart';
