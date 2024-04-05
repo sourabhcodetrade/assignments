38b022ad5e95f9107358dd67916fd69b51bb077a
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-05 17:23:39 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/gridexample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gridexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // home: GridViewScreen(),
+      home: GridViewBuilderScreen(),
+    );
+  }
+}
+
+class GridViewScreen extends StatefulWidget {
+  const GridViewScreen({super.key});
+
+  @override
+  State<GridViewScreen> createState() => _GridViewScreenState();
+}
+
+class _GridViewScreenState extends State<GridViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "GridView",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: GridView.count(
+        children: [
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+          getItem(0),
+        ],
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+      ),
+    );
+  }
+
+  Widget getItem(int index) {
+    print('$index constructed');
+    return Container(
+      margin: const EdgeInsets.only(bottom: 2, left: 2, right: 2),
+      height: 300,
+      width: 100,
+      color: Colors.purple,
+      child: Center(
+        child: Text(
+          "${index + 1}",
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GridViewBuilderScreen extends StatefulWidget {
+  const GridViewBuilderScreen({super.key});
+
+  @override
+  State<GridViewBuilderScreen> createState() => _GridViewBuilderScreenState();
+}
+
+class _GridViewBuilderScreenState extends State<GridViewBuilderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "GridView",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: GridView.builder(
+          padding: EdgeInsets.all(8),
+          itemCount: 20,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return getItem(index);
+          },
+        ));
+  }
+
+  Widget getItem(int index) {
+    print('$index constructed');
+    return Container(
+      margin: const EdgeInsets.only(bottom: 2, left: 2, right: 2),
+      height: 300,
+      width: 100,
+      color: Colors.purple,
+      child: Center(
+        child: Text(
+          "${index + 1}",
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
