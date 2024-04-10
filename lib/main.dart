1cbcbec645d272911a61a09e1306146ba28d8264
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-10 20:23:16 +0530

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'home_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await AwesomeNotifications().initialize(
     null,
     [
