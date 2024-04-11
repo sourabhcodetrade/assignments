5490522caa54af9bd1a5398d4b930a2e0617d191
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-11 09:27:02 +0530

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local/local_notification.dart';
 
@@ -5,6 +6,15 @@ import 'home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: const FirebaseOptions(
+      //   apiKey: 'AIzaSyAQjEgN2n22yyQVBFinR3AaRxrFeajNuTc',
+      //   appId: 'com.example.mvc',
+      //   messagingSenderId: 'sendid',
+      //   projectId: 'mvcapp-540be',
+      //   storageBucket: 'mvcapp-540be.appspot.com',
+      // )
+      );
   await LocalNotification.init();
 
   runApp(const MyApp());
