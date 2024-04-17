5fdcbda675e7c3ba2878aa277cce43d77831cff3
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-04-17 15:22:39 +0530

    Initial Commit

---
+++ lib/api.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:isolate';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+
+@pragma('vm:entry-point')
+void getData(SendPort sendPort) async {
+  var dataList;
+  try {
+    const url = 'https://superhero-search.p.rapidapi.com/api/heroes';
+    const headers = {
+      'X-RapidAPI-Key': '4f45c1c748mshac8fd3b453b345bp11488ajsnc197908c7307',
+      'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com'
+    };
+
+    Response response = await Dio().get(
+      url,
+      options: Options(
+        headers: headers,
+      ),
+    );
+
+    //
+    if (response.statusCode == 200) {
+      dataList = jsonDecode(response.data);
+      sendPort.send(dataList);
+    } else {
+      debugPrint("Something went wrong");
+      sendPort.send("Something went wrong");
+    }
+  } on DioException catch (e) {
+    debugPrint('Error fetching data: $e');
+    sendPort.send('Error fetching data: $e');
+  } catch (e) {
+    debugPrint('Error fetching data: $e');
+    sendPort.send('Error fetching data: $e');
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,82 @@
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:flutter_isoolate/api.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List dataList = [];
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  void fetchData() async {
+    ReceivePort receiverPort = ReceivePort();
+    Isolate.spawn(getData, receiverPort.sendPort);
+    dataList = await receiverPort.first;
+    setState(() {});
+    print("Isolate Result = $dataList");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              "Super heroes",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            dataList.isEmpty
+                ? const SizedBox()
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: dataList.length,
+                      itemBuilder: (context, index) => Card(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 16),
+                        child: ListTile(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          leading: CircleAvatar(
+                            radius: 25,
+                            backgroundImage: NetworkImage(
+                              dataList[index]['images']['lg'],
+                            ),
+                          ),
+                          title: Text(dataList[index]['name'].toString()),
+                          trailing: Text(
+                              "Strength: ${dataList[index]['powerstats']['strength']}"),
+                        ),
+                      ),
+                    ),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
