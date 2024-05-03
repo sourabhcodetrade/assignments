b47b4ce808c05fd0dc4e95099f5aff64e5c6986b
Sourabh Sharma <sourabh.sharma@mail.codetrade.io>
2024-05-03 17:05:18 +0530

    Added 2 more screen to show all users

---
+++ lib/app/modules/choice/choice_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_chat_application/app/utils/routes/routes.dart';
+
+class ChoiceScreen extends StatelessWidget {
+  const ChoiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              elevation: 4.0,
+              child: ListTile(
+                onTap: () {
+                  Navigator.pushNamed(context, Routes.infoScreen);
+
+                },
+                leading: const CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Icon(
+                    Icons.people,
+                    color: Colors.white,
+                  ),
+                ),
+                title: const Text(
+                  'See All Users',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+                trailing: const Icon(Icons.arrow_forward),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              elevation: 4.0,
+              child: ListTile(
+                onTap: () {
+                  Navigator.pushNamed(context, Routes.chatScreen);
+
+                },
+                leading: const CircleAvatar(
+                  backgroundColor: Colors.green,
+                  child: Icon(
+                    Icons.chat,
+                    color: Colors.white,
+                  ),
+                ),
+                title: const Text(
+                  'Go to Chat Screen',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+                trailing: const Icon(Icons.arrow_forward),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
